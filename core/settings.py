aca82974d5874dd2e37318a82dc590b9c4589d55
Tanisha-negi <tan.negi19@gmail.com>
2026-03-15 17:39:12 +0530

    Fix CORS middleware order and allow all origins

--- core/settings.py
+++ core/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',     
+    'corsheaders.middleware.CorsMiddleware',           
+    'whitenoise.middleware.WhiteNoiseMiddleware',        
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +121,4 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOWED_ORIGINS = [
-    "https://your-frontend-link.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
